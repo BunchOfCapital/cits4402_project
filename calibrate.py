a268b3d66d9114c9cd9115e2fe4c4b6c4934900b
Joel Wildman <joel.l.wildman@gmail.com>
2023-04-26 00:25:25 +0800

    add additional layers culling potential regions
    added checks for potential targets which check size, roundness and adherence to ellipses with other nearby potential targets

--- calibrate.py
+++ calibrate.py
@@ -2,11 +2,13 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import tkinter as tk
 import cv2
 import tkinter.filedialog as filedialog
 from PIL import ImageTk, Image
 import skimage
+import math
 
 
 from skimage import filters, segmentation, measure, morphology, color, util
@@ -26,6 +28,13 @@ def load_image():
 	return input_image
 
 image = load_image()
+
+def distance(source, dest):
+	# xdist = abs(source[0] - dest[0])
+	# ydist = abs(source[1] - dest[1])
+	# pythag_dist = math.sqrt(xdist**2 + ydist**2)
+	dist = np.linalg.norm(np.array(source) - np.array(dest))
+	return dist
 
 #colour boundaries in HSV space:
 blue_bounds = ([90,50,50], [150, 255, 255])
@@ -56,12 +65,89 @@ for target in bounds:
 	#and record it for the final mask
 	final_mask = cv2.bitwise_or(final_mask, mask)
 
-	#remove blobs that are too big
-
-	cv2.imshow("Filtered Image", np.hstack([image,filtered]))
-	cv2.waitKey(0)
+	#cv2.imshow("Filtered Image", np.hstack([image,filtered]))
+	#cv2.waitKey(0)
 
 
-filtered = cv2.bitwise_and(image, image, mask=final_mask)
-cv2.imshow("Filtered Image", np.hstack([image,filtered]))
-cv2.waitKey(0)
+#use the mask to label connected regions
+labelled = measure.label(final_mask)
+
+image_label_overlay = color.label2rgb(labelled, image=image, bg_label=0)
+ig, ax = plt.subplots(figsize=(10, 6))
+ax.imshow(image_label_overlay)
+
+#specify values to exclude accidentally labelled objects
+min_area = 30
+max_area = 250
+
+axis_ratio = 0.5
+
+ellipse_threshold = 1
+
+#array for storing accepted regions
+final_regions = []
+phase1_regions = []
+
+
+#cull pool of possible regions
+for region in measure.regionprops(labelled):
+
+    # take regions with reasonably sized areas
+	if region.area >= min_area and region.area <= max_area:
+
+		#make our division safe
+		if (region.axis_major_length != 0 and region.axis_minor_length):
+
+			#disregard regions that are too far from round by finding the ratio of their eigenvalues on the minor and major axes
+			if (math.sqrt(region.inertia_tensor_eigvals[1])/math.sqrt(region.inertia_tensor_eigvals[0]) > axis_ratio):
+				#print(region.inertia_tensor_eigvals)
+
+				#now we have an array with more realistic regions
+				phase1_regions.append(region)
+
+
+
+centroid_coords = []
+#find the closest 5 regions to main_region
+for main_region in phase1_regions:
+	main_centroid = main_region.centroid
+	centroid_coords.append(main_centroid)
+	distances = []
+
+	for close_region in phase1_regions:
+		#list the distances to all nodes
+		close_centroid = close_region.centroid
+		distances.append(distance(main_centroid, close_centroid))
+
+	#get indices of the closest 5
+	neighbours = np.argsort(distances)[:5]
+
+	#get their coordinates (and add the current region to complete the set)
+	close_coords = []
+	for region in neighbours:
+		close_coords.append(phase1_regions[region].centroid)
+
+	#match elipse
+	ellipse = measure.EllipseModel()
+	if (ellipse.estimate(np.array(close_coords))):
+		data_residuals = ellipse.residuals(np.array(close_coords))
+
+	#check if largest residual error value is above acceptable threshold
+	if (max(data_residuals) < ellipse_threshold):
+		final_regions.append(main_region)
+
+
+for region in final_regions:
+	# take the bounding box from regionprops to draw rectangles
+	min_row, min_col, max_row, max_col = region.bbox
+	rectangles = mpatches.Rectangle((min_col, min_row), max_col - min_col, max_row - min_row, 
+		fill=False, edgecolor='red', linewidth=1)
+	ax.add_patch(rectangles)
+
+ax.set_axis_off()
+plt.tight_layout()
+plt.show()
+
+# filtered = cv2.bitwise_and(image, image, mask=final_mask)
+# cv2.imshow("Filtered Image", np.hstack([image,filtered]))
+# cv2.waitKey(0)
